5f7c8b26d76b154eaf546e040be698356fdcd9ab
Krishan Kumar <kkrishan7074@gmail.com>
2021-07-18 11:27:12 +0530

    Changed app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-document.querySelector('html');
+//Define UI Vars
+
+const form= document.querySelector('#task-form');
+const taskList= document.querySelector('.collection');
+const clearBtn= document.querySelector('.clear-tasks');
+const filter= document.querySelector('#filter');
+const taskInput= document.querySelector('#task');
+
+//Load All Event Listners
+ 
+ loadEventListeners();
