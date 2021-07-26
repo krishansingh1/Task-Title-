f5bcb525a6a0cdd8c1c3edcdede30e52d0824b84
Krishan Kumar <kkrishan7074@gmail.com>
2021-07-26 18:57:20 +0530

    modified app.js

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ function loadEventListeners() {
 
      //Clear Task event
       clearBtn.addEventListener('click', clearTasks);
+
+     //Filter Task Event
+      filter.addEventListener('keyup',filterTasks);
 }
 
 //Add task
@@ -72,3 +75,19 @@ function addTask(e) {
        taskList.removeChild(taskList.firstChild);
      }
    } 
+
+  //Filter Tasks
+    function filterTasks(e){
+
+      const text= e.target.value.toLowerCase();
+
+      document.querySelectorAll('.collection-item').forEach(function(task) {
+        
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+           task.style.display= 'block';
+        } else{
+           task.style.display= 'none';
+        }
+      });
+    }
